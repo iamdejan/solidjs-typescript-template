37170058c5acdf310656c26450df4b29c9f8117e
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2025-03-23 11:07:03 +0700

    chore: add eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import solid from "eslint-plugin-solid/configs/typescript";
 import * as tsParser from "@typescript-eslint/parser";
 import globals from "globals";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
   js.configs.recommended,
@@ -19,4 +20,5 @@ export default [
       },
     },
   },
+  eslintPluginPrettierRecommended,
 ];
--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,29 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-import eslint from 'vite-plugin-eslint';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import eslint from "vite-plugin-eslint";
 
 export default defineConfig({
   plugins: [
     solidPlugin(),
-    { // default settings on build (i.e. fail on error)
+    {
+      // default settings on build (i.e. fail on error)
       ...eslint(),
-      apply: 'build',
+      apply: "build",
     },
-    { // do not fail on serve (i.e. local development)
+    {
+      // do not fail on serve (i.e. local development)
       ...eslint({
         failOnWarning: false,
         failOnError: false,
       }),
-      apply: 'serve',
-      enforce: 'post'
-    }],
+      apply: "serve",
+      enforce: "post",
+    },
+  ],
   server: {
     port: 3000,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });
